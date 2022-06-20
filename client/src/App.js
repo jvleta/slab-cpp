fec907024b601cd871ba06a437a39590d957a782
Jimmy Leta <jimmy.leta@gmail.com>
2022-06-19 22:37:48 -0400

    Created some basic ui components with styled-components library

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import { FormGroup, Label, Input, Message } from "./Forms";
+
+const Body = styled.body`
+  background: white;
+`;
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Body>
+      <Title>Slabbath</Title>
+      <FormGroup>
+        <Label>Wind Speed</Label>
+        <Input id="windspeed" />
+        <Message>This is the validation message</Message>
+      </FormGroup>
+      <FormGroup>
+        <Label>Viscosity of Air</Label>
+        <Input id="airviscosity"/>
+        <Message>This is the validation message</Message>
+      </FormGroup>
+    </Body>
   );
 }
 
