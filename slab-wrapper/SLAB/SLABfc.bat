e34ba6c0d40d00d09f616cc55ea398b3942643bc
Jimmy Leta <jimmy.leta@gmail.com>
2022-07-04 22:19:57 -0400

    Removing extraneous files

--- slab-wrapper/SLAB/SLABfc.bat
+++
@@ -1,4 +0,0 @@
-FC outpr1 nuout1 > slabdiff1.txt
-FC outpr2 nuout2 > slabdiff2.txt
-FC outpr3 nuout3 > slabdiff3.txt
-FC outpr4 nuout4 > slabdiff4.txt
--- slab-wrapper/slab.js
+++
@@ -1,2 +0,0 @@
-const runSlab = () => "This function will execute Slab someday.";
-module.exports = { runSlab };
