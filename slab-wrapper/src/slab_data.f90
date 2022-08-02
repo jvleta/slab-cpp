02c53ed7de169a81bbff06ab021fd8e5bbca4641
Jimmy Leta <jimmy.leta@gmail.com>
2022-08-01 21:50:03 -0400

    committing progress with slab_data module

--- slab-wrapper/src/slab_data.f90
+++ slab-wrapper/src/slab_data.f90
@@ -1,7 +1,7 @@
 module slab_data
   implicit none
 
-  type(SlabInput)
+  type :: SlabInput
     integer :: idspl
     integer :: ncalc
     real(8) :: wms
@@ -30,4 +30,33 @@ module slab_data
     real(8) :: stab ! atmospheric stability class value
     real(8) :: ala ! inverse monin-obukhov length (1/m)
   end type
+contains
+  
+  subroutine echo_input(input)
+    type(SlabInput) :: input
+    integer         :: i
+      write (3,114) input%idspl,input%ncalc,input%wms,input%cps,input%tbp, &
+                    input%cmed0,input%dhe,input%cpsl,input%rhosl,input%spb, &
+                    input%spc,input%ts,input%qs,input%as,input%tsd,input%qtis, & 
+                    input%hs,input%tav,input%xffm,(i,input%zp(i),i=1,4)
+
+  114 format (///'problem input'//'  idspl =',5x,i5/'  ncalc =',5x,i5/ &
+      '  wms   =',f10.6/'  cps   =',f10.2/ &
+      '  tbp   =',f10.2/'  cmed0 =',f10.2/ &
+      '  dhe   =',f10.0/'  cpsl  =',f10.2/'  rhosl =',f10.2/ &
+      '  spb   =',f10.2/'  spc   =',f10.2/ &
+      '  ts    =',f10.2/'  qs    =',f10.2/'  as    =',f10.2/ &
+      '  tsd   =',f10.0/'  qtis  =',f10.2/'  hs    =',f10.2/ &
+      '  tav   =',f10.2/'  xffm  =',f10.2/('  zp(',i1,') =',f10.2)) 
+
+      write (3,115) input%z0,input%za,input%ua,input%ta,input%rh,input%stab
+  115 format ('  z0    =',f10.6/'  za    =',f10.2/'  ua    =',f10.2/ &
+      '  ta    =',f10.2/,'  rh    =',f10.2/,'  stab  =',f10.2) 
+
+      if (input%stab .eq. 0.) then
+      write (3,112) input%ala
+  112 format ('  ala   =',f10.4)
+      endif
+
+  end subroutine
 end module
