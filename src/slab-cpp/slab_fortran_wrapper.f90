a1f22c237976738a00d9c311ba3825d71ae11d03
Jimmy Leta <jimmy.leta@gmail.com>
2022-09-10 17:47:22 -0400

    Removed idspl from common block and into new custom type

--- src/slab-cpp/slab_fortran_wrapper.f90
+++ src/slab-cpp/slab_fortran_wrapper.f90
@@ -4,58 +4,58 @@ implicit none
 
 type SlabData
   ! Release Gas Properties
-  double precision :: source_gas_molecular_weight
-  double precision :: vapor_heat_capacity
-  double precision :: source_gas_temperature
-  double precision :: source_gas_density
-  double precision :: boiling_point_temperature
-  double precision :: liquid_mass_fraction
-  double precision :: liquid_heat_capacity
-  double precision :: heat_of_vaporization
-  double precision :: source_liquid_density
-  double precision :: saturation_pressure_constant_a
-  double precision :: saturation_pressure_constant_b
-  double precision :: saturation_pressure_constant_c
+  ! real(4) :: source_gas_molecular_weight
+  ! real(4) :: vapor_heat_capacity
+  ! real(4) :: source_gas_temperature
+  ! real(4) :: source_gas_density
+  ! real(4) :: boiling_point_temperature
+  ! real(4) :: liquid_mass_fraction
+  ! real(4) :: liquid_heat_capacity
+  ! real(4) :: heat_of_vaporization
+  ! real(4) :: source_liquid_density
+  ! real(4) :: saturation_pressure_constant_a
+  ! real(4) :: saturation_pressure_constant_b
+  ! real(4) :: saturation_pressure_constant_c
   
   ! Spill Characteristics
   integer :: spill_type
-  double precision :: mass_source_rate
-  double precision :: continuous_source_duration
-  double precision :: continuous_source_mass
-  double precision :: instantaneous_source_mass
-  double precision :: source_area
-  double precision :: vertical_vapor_velocity
-  double precision :: source_half_weight
-  double precision :: source_height
-  double precision :: horizontal_vapor_velocity
+  ! real(4) :: mass_source_rate
+  ! real(4) :: continuous_source_duration
+  ! real(4) :: continuous_source_mass
+  ! real(4) :: instantaneous_source_mass
+  ! real(4) :: source_area
+  ! real(4) :: vertical_vapor_velocity
+  ! real(4) :: source_half_weight
+  ! real(4) :: source_height
+  ! real(4) :: horizontal_vapor_velocity
   
   ! Field Parameters
-  double precision :: concentration_averaging_time
-  double precision :: concentration_measurement_height_1
-  double precision :: concentration_measurement_height_2
-  double precision :: concentration_measurement_height_3
-  double precision :: concentration_measurement_height_4
+  ! real(4) :: concentration_averaging_time
+  ! real(4) :: concentration_measurement_height_1
+  ! real(4) :: concentration_measurement_height_2
+  ! real(4) :: concentration_measurement_height_3
+  ! real(4) :: concentration_measurement_height_4
   
   ! Ambient Meteorological Properties
-  double precision :: ambient_air_molecular_properties
-  double precision :: ambient_air_heat_capacity
-  double precision :: ambient_air_density
-  double precision :: ambient_measurement_height
-  double precision :: ambient_atmospheric_pressure
-  double precision :: ambient_wind_speed
-  double precision :: ambient_temperature
-  double precision :: relative_humidity
-  double precision :: ambient_friction_velocity
-  double precision :: atmospheric_stability_class_value
-  double precision :: inverse_monin_obukhov_length
-  double precision :: surface_roughness_height
+  ! real(4) :: ambient_air_molecular_properties
+  ! real(4) :: ambient_air_heat_capacity
+  ! real(4) :: ambient_air_density
+  ! real(4) :: ambient_measurement_height
+  ! real(4) :: ambient_atmospheric_pressure
+  ! real(4) :: ambient_wind_speed
+  ! real(4) :: ambient_temperature
+  ! real(4) :: relative_humidity
+  ! real(4) :: ambient_friction_velocity
+  ! real(4) :: atmospheric_stability_class_value
+  ! real(4) :: inverse_monin_obukhov_length
+  ! real(4) :: surface_roughness_height
 
   ! Additional Parameters
-  integer :: substep_multiplier
-  integer :: num_calculation_substeps
-  double precision :: gravitational_constant
-  double precision :: gas_constant
-  double precision :: von_karman_constant
+  ! integer :: substep_multiplier
+  ! integer :: num_calculation_substeps
+  ! real(4) :: gravitational_constant
+  ! real(4) :: gas_constant
+  ! real(4) :: von_karman_constant
 end type
 contains
   
