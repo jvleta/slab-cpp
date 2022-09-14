e8d1e03c5f4636b0a912e13dcc77885426badb8f
Jimmy Leta <jimmy.leta@gmail.com>
2022-09-14 14:49:58 -0400

    qs

--- src/slab-cpp/slab_data.f90
+++ src/slab-cpp/slab_data.f90
@@ -18,7 +18,7 @@ type SlabData
   
   ! Spill Characteristics
   integer :: spill_type
-  ! real(4) :: mass_source_rate
+  real(4) :: mass_source_rate
   ! real(4) :: continuous_source_duration
   ! real(4) :: continuous_source_mass
   ! real(4) :: instantaneous_source_mass
