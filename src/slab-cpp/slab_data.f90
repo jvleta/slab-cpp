3983f905bbcc115c822bbb644c1537998ed2c04f
Jimmy Leta <jimmy.leta@gmail.com>
2022-09-14 14:40:40 -0400

    spc

--- src/slab-cpp/slab_data.f90
+++ src/slab-cpp/slab_data.f90
@@ -14,7 +14,7 @@ type SlabData
   real(4) :: source_liquid_density
   real(4) :: press_sat_const_a
   real(4) :: press_sat_const_b
-  ! real(4) :: saturation_pressure_constant_c
+  real(4) :: press_sat_const_c
   
   ! Spill Characteristics
   integer :: spill_type
