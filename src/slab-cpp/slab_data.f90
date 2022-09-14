2f3ae85ca957f4a5edab86ee35de4340b9e032d7
Jimmy Leta <jimmy.leta@gmail.com>
2022-09-14 10:23:28 -0400

    cpsl, spb

--- src/slab-cpp/slab_data.f90
+++ src/slab-cpp/slab_data.f90
@@ -9,11 +9,11 @@ type SlabData
   real(4) :: source_gas_density
   real(4) :: boiling_point_temperature
   real(4) :: liquid_mass_fraction
-  ! real(4) :: liquid_heat_capacity
+  real(4) :: liquid_heat_capacity
   real(4) :: heat_of_vaporization
   real(4) :: source_liquid_density
   ! real(4) :: saturation_pressure_constant_a
-  ! real(4) :: saturation_pressure_constant_b
+  real(4) :: saturation_pressure_constant_b
   ! real(4) :: saturation_pressure_constant_c
   
   ! Spill Characteristics
